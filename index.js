19de0afe91c0859691aea35e3f57663fc681fa79
MHNahib <MHNahib@users.noreply.github.com>
2022-02-16 17:06:13 +0600

    activate express.json()

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const helmet = require("helmet");
 const asyncError = require("express-async-errors");
+const db = require("./setup/db");
 // routers path
 const home = require("./routes/home");
 const about = require("./routes/about");
+const student = require("./routes/auth/student");
 
 const app = express();
 
 // middleware
 app.use(helmet());
+app.use(express.json());
 const error = require("./middleware/error");
 
 // routers
 app.use("/", home);
 app.use("/about", about);
+app.use("/auth/student", student);
 
 app.use(error);
 
