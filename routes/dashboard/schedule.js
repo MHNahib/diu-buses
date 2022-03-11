54d3c27fa9d7d4f18123a548384857c5f03094ed
MHNahib <MHNahib@users.noreply.github.com>
2022-03-11 16:42:23 +0600

    added with start and ending location with edit fix

--- routes/dashboard/schedule.js
+++ routes/dashboard/schedule.js
@@ -16,17 +16,54 @@ router.get("/", async (req, res) => {
 // add scadule view
 router.get("/add", async (req, res) => {
   const bus = await Bus.find().select("name").sort("name");
-  const route = await Route.find().sort("name").select("routeNo");
+  const route = await Route.find().sort("name");
   const driver = await Driver.find().sort("driverName").select("driverName");
+  // console.log(route);
 
-  res.render("createschedule", { bus, route, driver });
+  const rowList = [];
+  let location = [];
+
+  // store location
+  for (let i = 0; i < route.length; i++) {
+    for (let j = 0; j < route[i].busStoppages.length; j++) {
+      rowList.push(route[i].busStoppages[j]);
+    }
+  }
+
+  // removed duplicacy
+  rowList.forEach((c) => {
+    if (!location.includes(c)) {
+      location.push(c);
+    }
+  });
+  // console.log(location);
+
+  res.render("createschedule", { bus, route, driver, location });
 });
 
 // add new schadule
 router.post("/", async (req, res) => {
   let bus = await Bus.find().select("name").sort("name");
-  let route = await Route.find().sort("name").select("routeNo");
+  let route = await Route.find().sort("name");
   let driver = await Driver.find().sort("driverName").select("driverName");
+
+  const rowList = [];
+  let location = [];
+
+  // store location
+  for (let i = 0; i < route.length; i++) {
+    for (let j = 0; j < route[i].busStoppages.length; j++) {
+      rowList.push(route[i].busStoppages[j]);
+    }
+  }
+
+  // removed duplicacy
+  rowList.forEach((c) => {
+    if (!location.includes(c)) {
+      location.push(c);
+    }
+  });
+
   // validate request
   //   console.log(req.body);
   const { error } = scheduleValidation(req.body);
@@ -36,6 +73,7 @@ router.post("/", async (req, res) => {
       bus,
       route,
       driver,
+      location,
     });
 
   // check bus
@@ -60,6 +98,8 @@ router.post("/", async (req, res) => {
     phone: driver.phone,
     startTime: req.body.startTime,
     availabeSeats: bus.seats,
+    start: req.body.start,
+    destination: req.body.destination,
   });
 
   await schedule.save();
@@ -74,10 +114,100 @@ router.get("/:id", async (req, res) => {
   if (!schadule) return res.status(404).send(`Schedule not found`);
 
   const bus = await Bus.find().select("name").sort("name");
-  const route = await Route.find().sort("name").select("routeNo");
+  const route = await Route.find().sort("name");
   const driver = await Driver.find().sort("driverName").select("driverName");
 
-  res.render("editSchedule", { responce: schadule, bus, driver, route });
+  const rowList = [];
+  let location = [];
+
+  // store location
+  for (let i = 0; i < route.length; i++) {
+    for (let j = 0; j < route[i].busStoppages.length; j++) {
+      rowList.push(route[i].busStoppages[j]);
+    }
+  }
+
+  // removed duplicacy
+  rowList.forEach((c) => {
+    if (!location.includes(c)) {
+      location.push(c);
+    }
+  });
+
+  res.render("editSchedule", {
+    responce: schadule,
+    bus,
+    driver,
+    route,
+    location,
+  });
+});
+
+// edit schedule
+router.post("/:id", async (req, res) => {
+  // check bus is valid or not
+  const schadule = await Schedule.findById(req.params.id);
+  if (!schadule) return res.status(404).send(`Schedule not found`);
+
+  let bus = await Bus.find().select("name").sort("name");
+  let route = await Route.find().sort("name");
+  let driver = await Driver.find().sort("driverName").select("driverName");
+
+  const rowList = [];
+  let location = [];
+
+  // store location
+  for (let i = 0; i < route.length; i++) {
+    for (let j = 0; j < route[i].busStoppages.length; j++) {
+      rowList.push(route[i].busStoppages[j]);
+    }
+  }
+
+  // removed duplicacy
+  rowList.forEach((c) => {
+    if (!location.includes(c)) {
+      location.push(c);
+    }
+  });
+
+  console.log(req.body);
+  // validate request
+  const { error } = scheduleValidation(req.body);
+  if (error)
+    return res.status(400).render("createschedule", {
+      error: error.details[0].message,
+      bus,
+      route,
+      driver,
+      location,
+    });
+
+  // check bus
+  bus = await Bus.findById(req.body.busId);
+  if (!bus) return res.status(404).send(`Bus not found`);
+
+  route = await Route.findById(req.body.routeId);
+  if (!route) return res.status(404).send(`Route not found`);
+
+  driver = await Driver.findById(req.body.driverId);
+  if (!route) return res.status(404).send(`Driver not found`);
+
+  schadule.routeId = req.body.routeId;
+  schadule.routeName = route.routeName;
+  schadule.busStoppages = route.busStoppages;
+  schadule.busId = req.body.name;
+  schadule.busName = bus.name;
+  schadule.driverId = req.body.driverId;
+  schadule.driverName = driver.driverName;
+  schadule.phone = driver.phone;
+  schadule.startTime = req.body.startTime;
+  schadule.availabeSeats = bus.seats;
+  schadule.start = req.body.start;
+  schadule.destination = req.body.destination;
+  await schadule.save();
+
+  req.flash("success_msg", `Successfully updated.`);
+  res.redirect("/dashboard/schedule");
 });
 
 // delete schedule
