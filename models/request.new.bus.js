d10800eb6f9636403b9e1df55ad6da7985cf928b
MHNahib <MHNahib@users.noreply.github.com>
2022-03-12 17:26:52 +0600

    added request new bus model

---
+++ models/request.new.bus.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
+
+// schedule schema
+const requestNewBus = new mongoose.Schema({
+  bookedSchedule: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Schedules",
+  },
+  newSchedule: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Schedules",
+  },
+  routeName: {
+    type: String,
+  },
+  status: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const RequestNewBus = new mongoose.model("RequestNewBus", requestNewBus);
+
+module.exports.RequestNewBus = RequestNewBus;
