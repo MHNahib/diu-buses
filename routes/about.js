21cc405864e51a3e54762497f3a623548c460f42
MHNahib <MHNahib@users.noreply.github.com>
2022-03-13 22:58:25 +0600

    updated about view and route with assets

--- routes/about.js
+++ routes/about.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const user = require("../middleware/user");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("about");
+router.get("/", user, (req, res) => {
+  res.render("about");
 });
 
 module.exports = router;
