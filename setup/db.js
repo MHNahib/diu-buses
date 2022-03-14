48e181216c0c81cfd58eaef9807979f33c44c8eb
MHNahib <MHNahib@users.noreply.github.com>
2022-03-14 10:15:00 +0600

    updated with mongodb atlus linkWITH .env.sample

--- setup/db.js
+++ setup/db.js
@@ -1,6 +1,17 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
+// local
+// mongoose
+//   .connect("mongodb://localhost/diubuses")
+//   .then(() => console.log(`connected`))
+//   .catch((err) => console.log(err));
 mongoose
-  .connect("mongodb://localhost/diubuses")
-  .then(() => console.log(`connected`))
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`connected`);
+  })
   .catch((err) => console.log(err));
